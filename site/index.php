7d609bc2ff129a7c2d2f12544149ac47115e3891
Larissa Rabello <lari.rabello@gmail.com>
2016-04-20 11:53:03 -0300

    starting navs

---
+++ site/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Otimista</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="images/" type="image/png" />
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <ul class="list-inline">
+      <li><a href="#"> Ir para o Conteúdo (Alt+1)</a></li>
+      <li><a href="#"> Ir para o Menu (Alt+2)</a></li>
+    </ul>
+    <ul class="list-inline">
+      <li><a href="#">A+</a></li>
+      <li><a href="#">A-</a></li>
+    </ul>
+    <ul class="list-inline">
+      <li><a href="#">B</a></li>
+      <li><a href="#">W-</a></li>
+    </ul>
+    <ul class="list-inline">
+      <li><a href="#">Acessibilidade (Alt+3)</a></li>
+      <li><a href="#">Mapa do Site (Alt+4)</a></li>
+    </ul>
+  </nav>
+  <nav class="navbar navbar-default">
+    <ul class="list-inline">
+      <li><a href="#"> Tema A</a></li>
+      <li><a href="#"> Tema B</a></li>
+      <li><a href="#"> Tema C</a></li>
+      <li><a href="#"> Tema X</a></li>
+      <li><a href="#"> Tema Y</a></li>
+      <li><a href="#"> Tema Z</a></li>
+    </ul>
+  </nav>
+  <header>
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+      </div>
+    </div>
+  </header>
+  <div class="container">
+
+  </div>
+</body>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</html>
