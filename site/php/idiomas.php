65f179ed52c9aff97ec17f834fed0ed849c2d429
Larissa Rabello <lari.rabello@gmail.com>
2016-04-28 17:12:38 -0300

    starting php scripts

---
+++ site/php/idiomas.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rafael.ogrim
+ * Date: 27/04/2016
+ * Time: 19:59
+ */
+class idiomas
+{
+    private $idiomas = [
+        'pt' => [
+            'index' => [
+                'titulo' => 'Otimista',
+                'irparaconteudo' => 'Ir para o conteúdo',
+                'irparamenu' => 'Ir para o menu',
+                'acessibilidade' => 'Acessibilidade',
+                'mapadosite' => 'Mapa do Site'
+            ]
+        ],
+        'en' => [
+            'index' => [
+                'titulo' => 'Otimist',
+                'irparaconteudo' => 'Go to content',
+                'irparamenu' => 'Go to the menu',
+                'acessibilidade' => 'Acessibility',
+                'mapadosite' => 'Sitemap'
+            ]
+        ]
+    ];
+
+    function BuscaIdioma($idioma, $pagina)
+    {
+        return $this->idiomas[$idioma][$pagina];
+    }
+}
